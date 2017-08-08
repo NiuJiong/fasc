25c101870bfcd2bc5cb3f1ee92b7acd16cff3631
lgdcnm <18268815524@163.com>
2017-08-08 14:05:31 +0800

    a

---
+++ js/index.js
@@ -0,0 +1,36 @@
+$(function(){
+	$(".jia").click(function(){
+		var num = $(this).prev().html();
+		
+		num++;
+		if(num>=99){
+			num = 99;
+		}
+		$(this).prev().html(num);
+		sum();
+	});
+	$(".jian").click(function(){
+		var num = $(this).next().html();
+		num--;
+		if(num<=0){
+			num = 0;
+		}
+		$(this).next().html(num);
+	});
+	
+//	总价
+	function sum(){
+		var sum;
+		var one = $(".one").html() * $(".picone").html();
+		var two = $(".two").html() * $(".pictwo").html();
+		var three = $(".san").html() * $(".picthree").html();
+		console.log($(".san").html());
+		sum = one + two + three;
+		$(".pic span").html("￥"+sum+".00");
+	}
+	
+	
+	
+	
+	
+});
